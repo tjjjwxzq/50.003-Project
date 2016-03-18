d715ed0e4b194d4a746466c320420a86e70247b1
tanshunyu <shunyutan@hotmail.com>
2016-03-18 21:03:53 +0800

    Updated Player.cs

--- Yay-A-Mouse/Assets/Scripts/Player.cs
+++ Yay-A-Mouse/Assets/Scripts/Player.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Assets.Scripts
 {
     /// <summary>
     /// Class to contain player specific data, currently only contains the player's Abilities.
     /// </summary>
-    public class Player
+    public class Player : NetworkBehaviour
     {
         public Abilities Abilities; //<! The player's Abilities
+        public AbilityController abilityController;
+
+        // network score so other players can see your progress
+        [SyncVar]
+        public int score = 0;
+
+        // status showing which state is the mouse in (refer to Status enum)
+        public int status = 0;
+
+        // TODO: add in method to call list of abilities this player has
+
+        public GameObject button; // this button (any one of the ability buttons on the right) calls the generic method CmdActivateAbilities
+
+        public enum Status
+        {
+            Normal,        // index 0
+            Immunity,      // index 1
+            TreatsGalore,  // index 2
+            Fearless,      // index 3
+            FatMouse,      // index 4
+            ScaryCat,      // index 5
+            BeastlyBuffet, // index 6
+            Thief          // index 7
+        };
 
         /// <summary>
         /// Creates a new Player with a specified set of abilities.
@@ -30,6 +56,32 @@ namespace Assets.Scripts
             {
                 return new Player(Abilities.LevelOneAbilities);
             }
+        }
+
+        // check which status the mouse is in (by index in the enum above)
+        public int checkStatus()
+        {
+            int check = status;
+            return check;
+        }
+
+        // to activate the correponding abilities on button press
+        /*[Command] // by default on channel 0
+        public void CmdActivateAbilities(GameObject button)
+        {
+            abilityController.ActivateAbility(button.name());
+        }*/
+
+        void Update()
+        {
+            if (!isLocalPlayer)
+                return;
+
+            // check if the ability button is pressed
+            /*if (button.onPress())
+            {
+                CmdActivateAbilities(button); // this method is networked
+            }*/
         }
     }
 }
