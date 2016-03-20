860b007a45f84794c3350aed4f0f4d8f28dd2f75
tanshunyu <shunyutan@hotmail.com>
2016-03-20 19:41:49 +0800

    Added host/client capabilities

--- Yay-A-Mouse/Assets/Scripts/Player.cs
+++ Yay-A-Mouse/Assets/Scripts/Player.cs
@@ -13,20 +13,23 @@ namespace Assets.Scripts
     public class Player : NetworkBehaviour
     {
         private AbilityController abilityController;
+
         public string name;
 
-        public Abilities Abilities; //<! The player's Abilities
+        public Abilities abilities; //<! The player's Abilities
 
         // network score so other players can see your progress
+        // *** @junqi: to access this in other scripts/clients, first initialise Player player, followed by player.score
         [SyncVar]
         public int score = 0;
 
         // status showing which state is the mouse in (refer to Status enum)
+        // *** to access this in other scripts/clients, first initialise Player player, followed by player.status
+        [SyncVar]
         public int status = 0;
 
-        // TODO: add in method to call list of abilities this player has
-
-        public GameObject button; // this button (any one of the ability buttons on the right) calls the generic method CmdActivateAbilities
+        // *** this button (any one of the ability buttons on the right) calls the generic method CmdActivateAbilities
+        public GameObject button;
 
         public enum Status
         {
@@ -46,7 +49,7 @@ namespace Assets.Scripts
         /// <param name="abilities"></param>
         public Player(Abilities abilities)
         {
-            Abilities = abilities;
+            this.abilities = abilities;
         }
 
         /// <summary>
@@ -67,23 +70,34 @@ namespace Assets.Scripts
             return check;
         }
 
-        // to activate the correponding abilities on button press
-        /*[Command] // by default on channel 0
+        // to activate the correponding abilities on button press -> Command function is called from the client, but invoked on the server
+        // to call a method from the server to the client, use [ClientRpc] instead
+        // *** @junqi/jiayu: is this function already taken care of by AbilityControls?
+        [Command] // by default on channel 0
         public void CmdActivateAbilities(GameObject button)
         {
-            abilityController.ActivateAbility(button.name());
-        }*/
+            abilityController.ActivateAbility((AbilityName)System.Enum.Parse(typeof(AbilityName),button.name)); // i.e. use this to get AbilityName.Immunity when button.name is Immunity
+            status = 0; // change back to normal state
+        }
+
+        // method to call list of abilities this player has
+        // *** @junqi/jiayu: where's the option for the player to choose the abilities he/she wants such that we can initalise all players to have EmptyAbilities then set the leve of those they choose to be 1?
+        private List<Ability> getAbilities()
+        {
+            return abilities.GetListOfAbilities();
+        }
 
         void Update()
         {
             if (!isLocalPlayer)
                 return;
 
-            // check if the ability button is pressed
-            /*if (button.onPress())
+            // if player is not at normal state, activate correponding ability
+            if (checkStatus() == 0)
             {
-                CmdActivateAbilities(button); // this method is networked
-            }*/
+                // Command function is called from the client, but invoked on the server
+                CmdActivateAbilities(button);
+            }
         }
     }
 }
