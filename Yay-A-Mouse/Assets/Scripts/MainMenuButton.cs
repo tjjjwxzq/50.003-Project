84ac40eeea4174a422eac78ae415e9dd9201b949
tanshunyu <shunyutan@hotmail.com>
2016-03-21 19:29:42 +0800

    Added lobby and main menu scenes

---
+++ Yay-A-Mouse/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour {
+
+    Text buttonText;
+
+    // Use this for initialization
+    void Start () {
+        buttonText = transform.FindChild("Text").GetComponent<Text>();
+    }
+}
