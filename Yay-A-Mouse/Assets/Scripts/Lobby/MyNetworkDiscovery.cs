486a4cf1ca7a83418a5f2f4370bdc73226324ed0
tjjjwxzq <aquietimmanence@gmail.com>
2016-03-24 18:48:01 +0800

    Finish basic name prompting scene; imported Asset Store lobby example into Multiplayer Test

---
+++ Yay-A-Mouse/Assets/Scripts/Lobby/MyNetworkDiscovery.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+/// <summary>
+/// Overrides NetworkDiscovery base class
+/// to handle reception of broadcase message 
+/// from server on the host
+/// </summary>
+
+public class MyNetworkDiscovery : NetworkDiscovery{
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        NetworkLobbyManager.singleton.networkAddress = fromAddress;
+        NetworkLobbyManager.singleton.StartClient();
+    }
+}
+
---
+++ Yay-A-Mouse/Assets/Scripts/PromptName/NamePrompt.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NamePrompt : MonoBehaviour {
+
+    // not sure whether it's more efficient to assign through the editor
+    private InputField nameInput;
+    private Text promptText;
+  
+	// Use this for initialization
+	void Start () {
+        nameInput = GameObject.Find("NameInput").GetComponent<InputField>();
+        promptText = GameObject.Find("Prompt").GetComponent<Text>();
+	}
+
+    public void OnOKButton()
+    {
+        string name = nameInput.text;
+        if(name.Length > 0)
+        {
+            PlayerPrefs.SetString("Player Name", name);
+        }
+        else
+        {
+            promptText.text = "Please enter your name!";
+            nameInput.Select();
+        }
+
+
+
+    }
+	
+}
