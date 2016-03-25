4bbb22784ad20899dd785122622b5318bda1049e
jiayu <yijiayu@gmail.com>
2016-03-25 23:50:06 +0800

    Worked on ability upgrade screen
    Can't seem to get clicks on the buttons to work though

--- Yay-A-Mouse/Assets/Scripts/AbilityUpgradeController.cs
+++ Yay-A-Mouse/Assets/Scripts/AbilityUpgradeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class AbilityUpgradeController : MonoBehaviour
@@ -8,8 +9,12 @@ public class AbilityUpgradeController : MonoBehaviour
     private Text selectedAbilityName;
     private Text selectedAbilityDescription;
     private Text selectedAbilityCurrentLevel;
+    private Text selectedAbilityCurrentLevelDetails;
     private Text selectedAbilityNextLevel;
+    private Text selectedAbilityNextLevelDetails;
     private Button improveSelectedAbilityButton;
+
+    private Dictionary<AbilityName, GameObject> abilityIcons; 
 
     private Player player;
 
@@ -19,27 +24,69 @@ public class AbilityUpgradeController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-        // todo: create ability icons programatically and add onclick handlers
+	    player = new Player(Abilities.EmptyAbilities);
+	    selectedAbility = AbilityName.BeastlyBuffet;
+	    selectedAbilityLevel = player.PAbilities[selectedAbility].Level;
 
 	    selectedAbilityName = GameObject.Find("SelectedAbilityTitle").GetComponent<Text>();
 	    selectedAbilityDescription = GameObject.Find("SelectedAbilityDescription").GetComponent<Text>();
 	    selectedAbilityCurrentLevel = GameObject.Find("SelectedAbilityCurrentLevel").GetComponent<Text>();
+	    selectedAbilityCurrentLevelDetails = GameObject.Find("SelectedAbilityCurrentLevelDetails").GetComponent<Text>();
 	    selectedAbilityNextLevel = GameObject.Find("SelectedAbilityNextLevel").GetComponent<Text>();
+	    selectedAbilityNextLevelDetails = GameObject.Find("SelectedAbilityNextLevelDetails").GetComponent<Text>();
 	    improveSelectedAbilityButton = GameObject.Find("ImproveSelectedAbilityButton").GetComponent<Button>();
 
-	    player = new Player(Abilities.EmptyAbilities);
-        selectedAbility = AbilityName.Fearless;
-	    selectedAbilityLevel = player.PAbilities[selectedAbility].Level;
-	}
+	    abilityIcons = new Dictionary<AbilityName, GameObject>(7)
+        {
+            {AbilityName.Fearless, GameObject.Find("Fearless") },
+            {AbilityName.Immunity, GameObject.Find("Immunity") },
+            {AbilityName.TreatsGalore, GameObject.Find("TreatsGalore") },
+            {AbilityName.FatMouse, GameObject.Find("FatMouse") },
+            {AbilityName.ScaryCat, GameObject.Find("ScaryCat") },
+            {AbilityName.BeastlyBuffet, GameObject.Find("BeastlyBuffet") },
+            {AbilityName.Thief, GameObject.Find("Thief") }
+        };
+
+        foreach (AbilityName ability in Enum.GetValues(typeof(AbilityName)))
+        {
+            abilityIcons[ability].GetComponentInChildren<Text>().text = player.PAbilities[ability].Level.ToString();
+            // todo: can't get the button to work
+            var ability1 = ability;
+            abilityIcons[ability].GetComponent<Button>().onClick.AddListener(() => selectedAbility = ability1);
+        }
+
+        // even this doesn't work
+        improveSelectedAbilityButton.onClick.AddListener(() => Debug.Log("HI"));
+
+        // todo: proper listener for improveSelectedAbilityButton
+        // todo: two points to assign
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        // todo: create data structure to hold strings
-
 	    selectedAbilityName.text = selectedAbility.ToString();
-	    selectedAbilityDescription.text = "SELECTED ABILITY DESC";
-	    selectedAbilityCurrentLevel.text = player.PAbilities[selectedAbility].Level.ToString();
-	    selectedAbilityNextLevel.text = (player.PAbilities[selectedAbility].Level + 1).ToString();
+	    selectedAbilityDescription.text = player.PAbilities[selectedAbility].Description;
+
+        selectedAbilityCurrentLevel.text = "Current level " + player.PAbilities[selectedAbility].Level.ToString();
+        if (player.PAbilities[selectedAbility].Level == 0)
+	    {
+	        selectedAbilityCurrentLevelDetails.enabled = false;
+	    }
+	    else
+	    {
+            selectedAbilityCurrentLevelDetails.text = player.PAbilities[selectedAbility].GetDetails();
+        }
+  
+	    if (player.PAbilities[selectedAbility].Level == player.PAbilities[selectedAbility].MaxLevel)
+	    {
+	        selectedAbilityNextLevel.enabled = false;
+	        selectedAbilityNextLevelDetails.enabled = false;
+	    }
+	    else
+	    {
+	        selectedAbilityNextLevel.text = "Next level " + (player.PAbilities[selectedAbility].Level + 1).ToString();
+	        selectedAbilityNextLevelDetails.text = player.PAbilities[selectedAbility].GetDetails();
+	    }
 	}
 }
